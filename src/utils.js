ab2bd7d4ae72fef133984696d6f6b70592fc7eff
MrGibbit <m0nst3r.sl4y3r@gmail.com>
2026-02-14 15:24:01 -0500

    Delete src directory

--- src/utils.js
+++
@@ -1,4 +0,0 @@
-// src/utils.js
-export const clamp = (n, a, b) => Math.max(a, Math.min(b, n));
-export const dist  = (ax, ay, bx, by) => Math.hypot(ax - bx, ay - by);
-export const now   = () => performance.now();
